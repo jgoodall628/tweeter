ce4cc5085c1dec315d4638e0ff00181e4cd10c4f
Jeffrey Goodall <jgoodall628@gmail.com>
2015-09-15 07:45:55 -0500

    added avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909155403) do
+ActiveRecord::Schema.define(version: 20150915004403) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "message"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20150909155403) do
     t.text     "bio"
     t.string   "location"
     t.text     "following"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
